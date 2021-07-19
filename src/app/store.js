02ec6399b63627455b0b1c91bf5498bc54c6884b
Alex <alextownleymakes@gmail.com>
2021-07-19 16:17:32 -0500

    reading/writing to/from localstorage so todos persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from '../features/todoSlice'
+import { loadState, saveState } from '../features/localStorage';
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer
   },
+  preloadedState
+});
+
+store.subscribe(() => {
+  saveState({
+    todos: store.getState().todos
+  });
 });
